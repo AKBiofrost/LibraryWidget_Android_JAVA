7e5f51f42b46819976137c302e5b02df9a3a0380
Bivfrost <cedt00@gmail.com>
2025-02-09 18:08:35 +0100

    base de codigo con modulos de cada widget

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,10 @@ dependencyResolutionManagement {
 
 rootProject.name = "LibraryWidget_Android_JAVA"
 include(":app")
- 
+include(":app:BottomSheetDialog")
+include(":app:BottomSheetDialogFragment_imagen")
+include(":app:BottomSheetDialogFragment_ListCardView")
+include(":app:AlertDialogView")
+include(":app:BottomSheetDialogFragment_Notification")
+include(":app:ExpandableListView")
+include(":app:ExpandableListView_itemCardView")
